2bd96312480163e6436db72183f6070fc8e44986
Евгений Протозанов <weirdcarrotmonster@gmail.com>
2012-02-23 12:26:17 +0400

    Отрисовка юнитов только в видимой области карты

--- CMap.h
+++ CMap.h
@@ -136,10 +136,14 @@ void CMap::DrawTiles(SDL_Surface *Surf_Display, int MapX, int MapY)
 void CMap::DrawUnits(SDL_Surface *Surf_Display, int MapX, int MapY)
 {
     for(int i = 0; i < UnitList.size(); i++) {
-        CSurface::OnDraw(Surf_Display, Body_Tileset,
-                         MapX + (UnitList[i].posx - leftedgepositionx) * TILE_SIZE,
-                         MapY + (UnitList[i].posy - leftedgepositiony) * TILE_SIZE,
-                         0, 0, TILE_SIZE, TILE_SIZE);
+        if ( UnitList[i].posx >= leftedgepositionx &&
+             UnitList[i].posy >= leftedgepositiony &&
+             UnitList[i].posx <= SCREEN_WIDTH + leftedgepositionx &&
+             UnitList[i].posy <= SCREEN_HEIGHT + leftedgepositiony)
+            CSurface::OnDraw(Surf_Display, Body_Tileset,
+                             MapX + (UnitList[i].posx - leftedgepositionx) * TILE_SIZE,
+                             MapY + (UnitList[i].posy - leftedgepositiony) * TILE_SIZE,
+                             0, 0, TILE_SIZE, TILE_SIZE);
     }
 }
 
